9b88bbbafb2c75c0bc06d758c19520c8b7222040
pietro.bastos97@gmail.com <pietro.bastos97@gmail.com>
2025-11-18 19:44:13 -0300

    Final do Projeto

--- Main.java
+++ Main.java
@@ -1,19 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) {
+        List<Passageiro> listaPassageiros = new ArrayList<>();
+        int opcao = -1;
         String nomeAeroporto = JOptionPane.showInputDialog("Digite o nome do aeroporto:");
         String capacidadeStr = JOptionPane.showInputDialog("Digite a capacidade de passageiros:");
-        int capacidade = Integer.parseInt(capacidadeStr);
+        if (capacidadeStr == null || capacidadeStr.trim().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Capacidade invalida");
+            return;
+        }
+        try {
+            int capacidade = Integer.parseInt(capacidadeStr.trim());
+            Aeroporto aeroporto = new Aeroporto(nomeAeroporto, capacidade);
+            while(opcao != 0){
+            String nomePassageiro = JOptionPane.showInputDialog("Digite o nome do passageiro:");
+            String vooPassageiro = JOptionPane.showInputDialog("Digite o voo do passageiro:");
+            String assentoPassageiro = JOptionPane.showInputDialog("Digite o assento do passageiro:");
+            int assentoIntPassageiro = Integer.parseInt(assentoPassageiro);
 
-        Aeroporto aeroporto = new Aeroporto(nomeAeroporto, capacidade);
+            Passageiro p = new Passageiro(nomePassageiro, vooPassageiro,assentoIntPassageiro);
+            listaPassageiros.add(p);
+            opcao = Integer.parseInt(JOptionPane.showInputDialog("DEseja adicionar outro passageiro? "));
 
-        String nomePassageiro = JOptionPane.showInputDialog("Digite o nome do passageiro:");
-        String vooPassageiro = JOptionPane.showInputDialog("Digite o voo do passageiro:");
-        Passageiro p = new Passageiro(nomePassageiro, vooPassageiro);
-
-        aeroporto.adicionarPassageiro(p, 0);
-
-        JOptionPane.showMessageDialog(null, aeroporto.listarPassageiros());
+            
+            
+            }
+            
+            
+            
+            JOptionPane.showMessageDialog(null, "Aeroporto: " + aeroporto.getNomeAeroporto() + "\n");
+            JOptionPane.showMessageDialog(null, "\n"+listaPassageiros);
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
     }
 }
